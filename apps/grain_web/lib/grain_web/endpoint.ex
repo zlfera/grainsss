b14a3a0f3c69b3bf52a004ca3ac897a2e717de9b
zlfera <zlfera@gmail.com>
2018-11-10 01:26:36 +0800

    up

--- apps/grain_web/lib/grain_web/endpoint.ex
+++ apps/grain_web/lib/grain_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule GrainWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :grain_web,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
