6bd71b5b285ffb53f85c6541d8a480dabe7f5336
zlfera <zlfera@gmail.com>
2018-11-14 09:03:32 +0800

    up

--- apps/grain/lib/grain/application.ex
+++ apps/grain/lib/grain/application.ex
@@ -2,14 +2,15 @@ defmodule Grain.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-  use Supervisor
+  # use Supervisor
   use Application
 
   def start(_type, _args) do
     children = [
       Grain.Repo,
       # worker(Grain.Task, []),
-      worker(Grain.Scheduler, [])
+      # worker(Grain.Scheduler, []),
+      Grain.Scheduler
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Grain.Supervisor)
