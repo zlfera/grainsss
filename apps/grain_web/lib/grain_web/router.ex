f56ac75115a9a32bd39b16952d86063bedb06baf
zlfera <zlfera@gmail.com>
2018-11-16 06:31:17 +0800

    up

--- apps/grain_web/lib/grain_web/router.ex
+++ apps/grain_web/lib/grain_web/router.ex
@@ -17,6 +17,7 @@ defmodule GrainWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/grains", GrainController, :index
     post "/grains", GrainController, :index
   end
 
