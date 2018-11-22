9cefa08bf9949b453218104508b2bfffbff3763f
zlfera <zlfera@gmail.com>
2018-11-23 05:38:02 +0800

    up

--- apps/grain/lib/grain/grains/grains.ex
+++ apps/grain/lib/grain/grains/grains.ex
@@ -6,11 +6,54 @@ defmodule Grain.Grains do
   import Ecto.Query
 
   alias Grain.Grains.Grain, as: Ggg
+  alias Grain.Repo, as: Gr
 
   def list_grains do
     Ggg
     # |> Ecto.Query.where([l], l.latest_price != "0")
     |> Ecto.Query.order_by(desc: :inserted_at)
     |> Grain.Repo.all()
+  end
+
+  def grains_list(x, y) do
+    case y do
+      "latest_price" ->
+        Ggg
+        |> Ecto.Query.where([l], l.latest_price != "0")
+        |> Ecto.Query.order_by(desc: :inserted_at)
+        |> Grain.Repo.all()
+
+      "starting_price" ->
+        [f] = Ecto.Query.from(p in Ggg, select: min(p.starting_price)) |> Gr.all()
+
+        Ggg
+        |> Ecto.Query.where(starting_price: ^f)
+        |> Ecto.Query.where([g], g.latest_price != "0")
+        |> Ecto.Query.order_by(desc: :inserted_at)
+        |> Gr.all()
+
+      "year" ->
+        Ggg
+        |> Ecto.Query.where(year: ^x)
+        |> Ecto.Query.where([g], g.latest_price != "0")
+        |> Ecto.Query.order_by(desc: :inserted_at)
+        |> Gr.all()
+
+      "address" ->
+        [xx, _] = x |> String.split("(")
+
+        Ggg
+        |> Ecto.Query.where(address: ^xx)
+        |> Ecto.Query.where([g], g.latest_price != "0")
+        |> Ecto.Query.order_by(desc: :inserted_at)
+        |> Gr.all()
+
+      "variety" ->
+        Ggg
+        |> Ecto.Query.where(variety: ^x)
+        |> Ecto.Query.where([g], g.latest_price != "0")
+        |> Ecto.Query.order_by(desc: :inserted_at)
+        |> Gr.all()
+    end
   end
 end
--- apps/grain_web/lib/grain_web/controllers/grain_controller.ex
+++ apps/grain_web/lib/grain_web/controllers/grain_controller.ex
@@ -1,56 +1,14 @@
 defmodule GrainWeb.GrainController do
   use GrainWeb, :controller
   alias Grain.Grains, as: Gg
-  alias Grain.Grains.Grain, as: Ggg
-  alias Grain.Repo, as: Gr
-  import Ecto.Query
 
   def index(conn, params) do
     grains =
-      if !Map.has_key?(params, "td_data") do
-        Gg.list_grains()
-      else
+      if Map.has_key?(params, "td_data") do
         [x, y] = params["td_data"] |> String.split(",")
-
-        case y do
-          "latest_price" ->
-            Ggg
-            |> Ecto.Query.where([l], l.latest_price != "0")
-            |> Ecto.Query.order_by(desc: :inserted_at)
-            |> Grain.Repo.all()
-
-          "starting_price" ->
-            [f] = Ecto.Query.from(p in Ggg, select: min(p.starting_price)) |> Gr.all()
-
-            Ggg
-            |> Ecto.Query.where(starting_price: ^f)
-            |> Ecto.Query.where([g], g.latest_price != "0")
-            |> Ecto.Query.order_by(desc: :inserted_at)
-            |> Gr.all()
-
-          "year" ->
-            Ggg
-            |> Ecto.Query.where(year: ^x)
-            |> Ecto.Query.where([g], g.latest_price != "0")
-            |> Ecto.Query.order_by(desc: :inserted_at)
-            |> Gr.all()
-
-          "address" ->
-            [xx, _] = x |> String.split("(")
-
-            Ggg
-            |> Ecto.Query.where(address: ^xx)
-            |> Ecto.Query.where([g], g.latest_price != "0")
-            |> Ecto.Query.order_by(desc: :inserted_at)
-            |> Gr.all()
-
-          "variety" ->
-            Ggg
-            |> Ecto.Query.where(variety: ^x)
-            |> Ecto.Query.where([g], g.latest_price != "0")
-            |> Ecto.Query.order_by(desc: :inserted_at)
-            |> Gr.all()
-        end
+        Gg.grains_list(x, y)
+      else
+        Gg.list_grains()
       end
 
     render(conn, "index.html", grains: grains)
