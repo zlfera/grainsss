9e1ed62cda99bdc1cc2e93ac46a75ffdc4ce4840
zlfera <zlfera@gmail.com>
2018-11-26 10:52:13 +0800

    up

--- apps/grain/config/config.exs
+++ apps/grain/config/config.exs
@@ -10,10 +10,11 @@ config :grain,
 
 config :grain, Grain.Scheduler,
   jobs: [
-    # {{:extended, "*/10 * 0-3/1 * *"}, {Grain.Tasks, :run, [pid]}}
+    {{:extended, "*/10 0/30 0-3/1 * *"}, {Grain.Tasks, :run, [pid]}},
+    {{:extended, "*/10 30/30 0-3/1 * *"}, {Grain.Tasks, :run, [pid]}}
     # {"* * * * *", {Grain.Tasks, :run, []}}
-    {"0/30 0-3/1 * * *", {Grain.Tasks, :run, [pid]}},
-    {"30/30 0-3/1 * * *", {Grain.Tasks, :run, [pid]}}
+    # {"0/30 0-3/1 * * *", {Grain.Tasks, :run, [pid]}},
+    # {"30/30 0-3/1 * * *", {Grain.Tasks, :run, [pid]}}
   ]
 
 import_config "#{Mix.env()}.exs"
