3db124d8fbbac617428227cadb8744caab86f4fe
zlfera <zlfera@gmail.com>
2018-12-25 11:52:19 +0800

    up

--- apps/grain/config/prod.secret.exs
+++ apps/grain/config/prod.secret.exs
@@ -12,8 +12,7 @@ use Mix.Config
 # Configure your database
 config :grain, Grain.Repo,
   # url: "${DATABASE_URL}",
-  # System.get_env("DATABASE_URL1"),
-  url:
-    "postgres://jglfexii:Oraq_oukbxRtsND6_6NMCo9nZz4g2Wnx@baasu.db.elephantsql.com:5432/jglfexii",
+  url: System.get_env("DATABASE_URL"),
+  # url: "postgres://jglfexii:Oraq_oukbxRtsND6_6NMCo9nZz4g2Wnx@baasu.db.elephantsql.com:5432/jglfexii",
   ssl: true,
   pool_size: 5
