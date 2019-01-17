191243055267c2261bc21c15d7e4e7f18c18c4a5
zlfera <zlfera@gmail.com>
2019-01-17 11:57:45 +0800

    up

--- apps/grain/lib/grain/tasks.ex
+++ apps/grain/lib/grain/tasks.ex
@@ -11,7 +11,7 @@ defmodule Grain.Tasks do
     Agent.get(pid, fn i -> i end) |> IO.inspect()
     p = Agent.get(pid, fn i -> i end)
 
-    if p != %{} && b() != [] do
+    if p != %{} do
       Map.keys(p) |> Enum.each(fn i -> Process.alive?(p[i]) |> IO.puts() end)
       IO.puts("当前任务正在进行中")
     else
