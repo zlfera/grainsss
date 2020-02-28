8a3101d56db7e58c011cdbe915e066212885f735
zlfera <zlfera@gmail.com>
2020-02-28 12:54:15 +0800

    up

--- apps/grain_web/config/prod.exs
+++ apps/grain_web/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # before starting your production server.
 config :grain_web, GrainWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 5000],
-  url: [host: "example.com", port: 80],
+  url: [host: "grain.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
