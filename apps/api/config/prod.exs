e36a8160556cb1e9f978b0b47e45f770078af94a
zlfera <zlfera@gmail.com>
2020-02-28 11:02:32 +0800

    up

--- apps/api/config/prod.exs
+++ apps/api/config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :api, ApiWeb.Endpoint,
-  url: [host: "example.com", port: 8080],
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
