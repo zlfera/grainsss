f66ab2ec85442fd5dec1337fc510642ca4610d39
zlfera <zlfera@gmail.com>
2020-02-28 11:20:05 +0800

    up

--- apps/grain_web/config/dev.exs
+++ apps/grain_web/config/dev.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :grain_web, GrainWeb.Endpoint,
-  http: [port: 5000],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
--- apps/grain_web/config/prod.exs
+++ apps/grain_web/config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :grain_web, GrainWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [:inet6, port: 5000],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
