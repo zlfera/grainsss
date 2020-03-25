9303141af5997802781bbe5f7070d5e09b23e83c
zlfera <zlfera@gmail.com>
2020-03-25 09:07:55 +0800

    up

--- apps/grain/mix.exs
+++ apps/grain/mix.exs
@@ -37,7 +37,7 @@ defmodule Grain.MixProject do
   defp deps do
     [
       {:postgrex, ">= 0.0.0"},
-      {:ecto_sql, "~> 3.4", override: true},
+      {:ecto_sql, "~> 3.4"},
       # {:httpotion, "~> 3.1"},
       {:httpoison, "~> 1.5"},
       {:quantum, "~> 2.3"},
