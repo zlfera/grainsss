0fdbc88c47a81fcfa2f760c4606eec3ffef385a0
zlfera <zlfera@gmail.com>
2020-04-03 18:48:14 +0800

    up

--- apps/grain/lib/grain/tasks.ex
+++ apps/grain/lib/grain/tasks.ex
@@ -3,7 +3,25 @@ defmodule Grain.Tasks do
   alias Grain.TaskGrain, as: Gt
 
   def run(pid) do
-    {:ok, _} = Application.ensure_all_started(:grain)
+    # {:ok, _} = Application.ensure_all_started(:grain)
+    a = Grain.Repo.all(Grain.Grains.Grain)
+
+    aa =
+      Enum.reject(a, fn x ->
+        Map.has_key?(x, :request_no) == false
+      end)
+
+    aaa =
+      Enum.reject(aa, fn x ->
+        x.request_no == nil
+      end)
+
+    Enum.each(aaa, fn x ->
+      year = Grain.TaskGrain.get_year(x.request_no)
+      p = Ecto.Changeset.change(x, year: year)
+      Grain.Repo.update!(p)
+    end)
+
     p = Agent.get(pid, & &1)
     IO.inspect(p)
 
