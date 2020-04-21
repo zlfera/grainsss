21bf4f2425fdbb3d187f53689a2310fc3c58b1cb
zlfera <zlfera@gmail.com>
2020-04-22 07:14:59 +0800

    up

--- apps/grain_web/lib/grain_web/views/grain_view.ex
+++ apps/grain_web/lib/grain_web/views/grain_view.ex
@@ -4,7 +4,6 @@ defmodule GrainWeb.GrainView do
 
   def the_begin_page(page_num) do
     page_num
-    # String.to_integer(params["page_num"])
   end
 
   def url(num, params) do
@@ -44,46 +43,9 @@ defmodule GrainWeb.GrainView do
     )
   end
 
-  def fenye(pid, params) do
-    fenye =
-      if params["fenye"] in ["", nil] do
-        1
-      else
-        params["fenye"] |> String.to_integer()
-      end
-
-    Agent.update(pid, fn x -> x + fenye end)
-    Agent.get(pid, fn x -> x end)
-  end
-
-  def fenyeend(params) do
-    if params["fenye"] in ["", nil] do
-      1
-    else
-      params["fenye"] |> String.to_integer()
-    end
-
-    # Agent.get(pid, fn x -> x end)
-  end
-
-  def fenyeurl(fenye, params) do
-    Agent.update(fenye, fn x -> x + 10 end)
-    f = Agent.get(fenye, fn x -> x end)
-
-    "/grains?page=#{params["page"]}&city1=#{params["city1"]}&city2=#{params["city2"]}&city3=#{
-      params["city3"]
-    }&year=#{params["year"]}&limit=#{params["limit"]}&fenye=#{f}"
-  end
-
   def dizeng(pid) do
     Agent.update(pid, fn i -> i + 1 end)
     Agent.get(pid, fn i -> i end)
-  end
-
-  def page(num, params) do
-    "/grains?page=#{num}&city1=#{params["city1"]}&city2=#{params["city2"]}&city3=#{
-      params["city3"]
-    }&year=#{params["year"]}&limit=#{params["limit"]}&fenye=#{params["fenye"]}"
   end
 
   def limit_num(num, params) do
