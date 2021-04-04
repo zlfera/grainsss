ad48f1472cbdcb5ffef5be9a6c852ccf845dcd58
zlfera <zlfera@gmail.com>
2021-04-04 21:16:23 +0800

    up

--- apps/grain_web/mix.exs
+++ apps/grain_web/mix.exs
@@ -38,13 +38,13 @@ defmodule GrainWeb.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.1"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix_live_view, "~> 0.15.0"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_ecto, "~> 4.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
-      {:telemetry_metrics, "~> 0.4"},
+      {:phoenix_live_dashboard, "~> 0.4.0"},
+      {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:grain, in_umbrella: true},
